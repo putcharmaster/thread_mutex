d4bdd404dbdba380e63d69889543a13b74b5f1cb
sangha <putcharmaster@gmail.com>
2024-11-20 00:21:20 +0100

    done for today qwerty is so annoying

--- pass_arg_thread.c
+++ pass_arg_thread.c
@@ -6,22 +6,30 @@
 int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
 void    *routine(void *arg){
+    // sleep(1);
     int index = *(int*)arg;
     printf("%d ", primes[index]);
+    // printf("%d \n", index);
+    free(arg);
+
 }
 
 int main(int ac, char **av){
     pthread_t th[10];
-    for (int i = 0; i < 10; i++){
-        if (pthread_create(&th[i], NULL, &routine, &i))
-            perror("Failed to create thread");
+    int i;
 
+    for (i = 0; i < 10; i++){
+        int *a = malloc(sizeof(int));
+        *a = i;
+        if (pthread_create(&th[i], NULL, &routine, a))
+            perror("Failed to create thread");
     }
-    for (int i = 0; i < 10; i++)
+    for (i = 0; i < 10; i++)
     {
         if (pthread_join(th[i], NULL))
             perror("Failed to join thread");
     }
     
+    printf("\n");
     return 0;
 }
